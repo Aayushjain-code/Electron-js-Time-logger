63be8b3b8d11299d93f7513ca18ab9c0acb6be80
Aayush Jain <aayushjain6020@gmail.com>
2023-01-19 00:31:02 +0530

    structural change

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,15 +1,15 @@
 /* eslint-disable import/order */
 import { createRoot } from 'react-dom/client';
-import App from './components/App';
 import './App.css';
 
 import { Provider } from 'react-redux';
 import store from './redux/app/store';
+import Main from './Main';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 root.render(
   <Provider store={store}>
-    <App />
+    <Main />
   </Provider>
 );
